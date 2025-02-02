851bc4c4842bd8200db491a7ee2a0a39479916c3
Sewook Yoo <42348669+Usaywook@users.noreply.github.com>
2025-02-03 00:52:02 +0900

    Time: 14 ms (57.01%), Space: 18.9 MB (27.85%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.py
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.py
@@ -5,7 +5,7 @@ class Solution:
         MAX = 10**5
         min_peak, max_peak = MAX + 1, -MAX - 1
         min_peak_idx, max_peak_idx = -1, N
-        start, end = 0, N-1
+        start, end = 1, 0
         for i in range(N-1):
             if nums[i] > nums[i+1] and nums[i+1] < min_peak:
                 min_peak = nums[i+1]
@@ -13,16 +13,11 @@ class Solution:
         for i in range(min_peak_idx, -1, -1):                
             if nums[i] > min_peak:        
                 start = i        
-        if min_peak_idx == -1:
-            return 0
-        elif min_peak_idx == N-1:
-            return N  
-        else:
-            for i in range(N-1, 0, -1):        
-                if nums[i] < nums[i-1] and nums[i-1] > max_peak:
-                    max_peak = nums[i-1]
-                    max_peak_idx = i-1                
-            for i in range(max_peak_idx, N):        
-                if nums[i] < max_peak:                    
-                    end = i        
-            return end - start + 1
+        for i in range(N-1, 0, -1):        
+            if nums[i] < nums[i-1] and nums[i-1] > max_peak:
+                max_peak = nums[i-1]
+                max_peak_idx = i-1                
+        for i in range(max_peak_idx, N):        
+            if nums[i] < max_peak:                    
+                end = i 
+        return end - start + 1
